b1db19336076097156cd59b03e9f3c619540185e
Chen <189380064829@163.com>
2024-09-08 01:25:58 +0800

    fix bug

--- modules/eks/auth.sh
+++ modules/eks/auth.sh
@@ -3,3 +3,10 @@ kubectl version
 
 rm -f ~/.kube/config
 aws eks update-kubeconfig --name $1
+
+# Update helm
+# Add eks-charts 仓库
+helm repo add eks https://aws.github.io/eks-charts
+# Add Karpenter 仓库
+helm repo add karpenter https://charts.karpenter.sh
+helm repo update
