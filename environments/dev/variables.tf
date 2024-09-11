9c452c202a1ec1f00778e4e91d96904a099c6ea9
Chen <189380064829@163.com>
2024-09-11 12:24:09 +0800

    add readme.

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "aws_profile_name" {
   description = "aws profile name"
   type        = string
-  default     = "glb"
+  default     = "default"
 }
 
 variable "project_name" {
