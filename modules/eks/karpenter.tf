b3325b0acf564e06fdc808edfba04f74d943593f
Chen <189380064829@163.com>
2024-09-19 11:18:27 +0800

    add monitor

--- modules/eks/karpenter.tf
+++ modules/eks/karpenter.tf
@@ -73,7 +73,6 @@ resource "helm_release" "karpenter" {
 ################################################################################
 
 
-
 resource "kubectl_manifest" "karpenter_provisioner" {
   yaml_body = <<-YAML
   apiVersion: karpenter.sh/v1alpha5
